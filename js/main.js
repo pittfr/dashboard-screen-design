66ac0ef0ab8bbf54a8f6e56b3b4732554ee732fa
Pitt <dpittmor@gmail.com>
2025-08-22 15:35:24 +0100

    Added interactivity to the toggles in sidebar-info

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-let sidebarMenuToggle = document.querySelector(".sidebar-menu__header .sidebar-menu__toggle");
-let sidebarMenuBurger = document.querySelector(".sidebar-menu__header .sidebar-menu__burger");
-
 const toggleSidebarMenu = () => {
     document.body.classList.toggle('menu-expanded');
 };
 
+const toggleSidebarInfo = () => {
+    document.body.classList.toggle('info-expanded');
+}
+
+// SIDEBAR-MENU
+
+let sidebarMenuToggle = document.querySelector(".sidebar-menu__toggle");
+let sidebarMenuBurger = document.querySelector(".sidebar-menu__burger");
+
+
 sidebarMenuToggle.addEventListener('click', toggleSidebarMenu);
 
 sidebarMenuBurger.addEventListener('click', toggleSidebarMenu);
+
+//SIDEBAR-INFO
+
+let sidebarInfoToggle = document.querySelector(".sidebar-info__toggle");
+let sidebarInfoFAB = document.querySelector(".sidebar-info__toggle-fab");
+
+sidebarInfoToggle.addEventListener('click', toggleSidebarInfo);
+sidebarInfoFAB.addEventListener('click', toggleSidebarInfo);
